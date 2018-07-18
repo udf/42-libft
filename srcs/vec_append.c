7bfea194b2ac70c202feeb8d4ff6008bad0c5059
udf <tabhooked@gmail.com>
2018-07-18 21:50:36 +0200

    fix wrong size calculation

--- srcs/vec_append.c
+++ srcs/vec_append.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   vec_append.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: anonymous <anonymous@student.42.fr>        +#+  +:+       +#+        */
+/*   By: mhoosen <mhoosen@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/27 16:13:01 by mhoosen           #+#    #+#             */
-/*   Updated: 2018/07/10 19:38:40 by anonymous        ###   ########.fr       */
+/*   Updated: 2018/07/18 21:11:48 by mhoosen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ size_t	vec_append(t_vec *v, void *item)
 {
 	if (!v)
 		return (0);
-	if (v->length * v->type_size >= v->mem_size)
+	if ((v->length + 1) * v->type_size >= v->mem_size)
 		if (!vec_reserve(v, MAX(2, v->length * 2)))
 			return (0);
 	ft_memcpy(v->data + v->length * v->type_size, item, v->type_size);
